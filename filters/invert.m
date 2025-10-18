5132c3f9a51780ea19139b124d90d77804d45440
Lina Amer <lina088@aucklanduni.ac.nz>
2025-10-19 08:57:22 +1300

    invert filter added

---
+++ filters/invert.m
@@ -0,0 +1,16 @@
+%Copy paste your Invert function implementation below.
+function [InvertedImgArray] = Invert(ImgArray)
+% The Invert function converts the supplied image (RGB or greyscale) to its
+% opposite colours(complementary hues) and returns the inverted image.
+%
+% Input: ImgArray, an 𝑚-by-𝑛-by-𝑘 uint8 array representing an RGB or
+% greyscale colour image.
+% Output: InvertedImgArray, an 𝑚-by-𝑛-by-𝑘 uint8 array representing the
+% inverted image.
+%
+% Author: Lina Amer
+
+%calculate the opposite colour for each pixel in the image
+InvertedImgArray=255-ImgArray;
+
+end
